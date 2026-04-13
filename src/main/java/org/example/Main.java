5715a5ed09f23298600a411692bda51b3f2ee836
Walaachabab <alwala20172017@gmail.com>
2026-04-13 17:20:42 +0300

    First commit for the new project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+        Square s1 = new Square(5.0, "Red", false);
+        System.out.println(s1.toString());
+        System.out.println("Area: " + s1.getArea());
+        System.out.println("Perimeter: " + s1.getPerimeter());
+
+        s1.setSide(10.0);
+        System.out.println("After resizing side to 10:");
+        System.out.println(s1.toString());
+
+
+    }
+}
