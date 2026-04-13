3af8e1b9ce25b18c5ac476f2443735bb8c61dc1c
Walaa_Alrashidi <alwala20172017@gmail.com>
2026-04-13 22:40:19 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,7 @@ package org.example;
 
 public class Main {
 
-
-
-
     public static void main(String[] args) {
-
-
         Square s1 = new Square(5.0, "Red", false);
         System.out.println(s1.toString());
         System.out.println("Area: " + s1.getArea());
